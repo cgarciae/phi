2dfe1d728fb39b72086ecb76a3c1755a7acd79c0
cgarciae <cgarcia.e88@gmail.com>
2017-06-13 11:26:40 -0500

    save

--- phi/api.py
+++ phi/api.py
@@ -1,5 +1,39 @@
 from .python_builder import P
 
+F = P.F
+
+
+#constants
+Val = P.Val
+
+#access
+Obj = P.Obj
+Rec = P.Rec
+
+#pipes
+Seq = P.Seq
+Pipe = P.Pipe
+
+#state
+Ref = P.Ref
+Read = P.Read
+Write = P.Write
+
+# flow control
+If = P.If
+With = P.With
+Context = P.Context
+
+# collections
+List = P.List
+Dict = P.Dict
+Set = P.Set
+Tuple = P.Tuple
+
+# special
+ReadList = P.ReadList
+
+# then
 Then0 = P.Then0
 Then = P.Then
 Then1 = P.Then1
@@ -8,18 +42,3 @@ Then3 = P.Then3
 Then4 = P.Then4
 Then5 = P.Then5
 ThenAt = P.ThenAt
-Read = P.Read
-Write = P.Write
-Val = P.Val
-Pipe = P.Pipe
-Obj = P.Obj
-Rec = P.Rec
-Context = P.Context
-With = P.With
-Ref = P.Ref
-Dict = P.Dict
-F = P.F
-ReadList = P.ReadList
-List = P.List
-Seq = P.Seq
-If = P.If
